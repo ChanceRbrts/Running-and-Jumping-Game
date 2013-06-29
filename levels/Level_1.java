2a951206e58a426dad9e5d0a95574f7ca0c73cb8
PlatPlat48 <roberts.chance@gmail.com>
2013-06-29 16:19:31 -0600

    Level One Data for Objects to Analyze/Test Room

---
+++ levels/Level_1.java
@@ -0,0 +1,27 @@
+package levels;
+
+public class Level_1 {
+  public String[] level;
+	public int width;
+	public int height;
+	public Level_1(){
+		width = 20;
+		height = 15;
+		level = new String[15];
+		level[0] =  "00000000000000000000";
+		level[1] =  "0SSSSSSSSSSSSSSSSSS0";
+		level[2] =  "0S0000000000000000S0";
+		level[3] =  "0S0000000000000000S0";
+		level[4] =  "0S0000000000000000S0";
+		level[5] =  "0S0000000000000000S0";
+		level[6] =  "0S00000000P0000000S0";
+		level[7] =  "0S0000000000000000S0";
+		level[8] =  "0S0000000000000000S0";
+		level[9] =  "0S000000SSSSS00000S0";
+		level[10] = "0S0000000000000000S0";
+		level[11] = "0S0000000000000000S0";
+		level[12] = "0S0000000000000000S0";
+		level[13] = "0SSSSSSSSSSSSSSSSSS0";
+		level[14] = "00000000000000000000";
+	}
+}
