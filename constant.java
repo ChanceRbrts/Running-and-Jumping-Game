eb06eff17da4bed8d724fc8ae4de265a2914252e
PlatPlat48 <roberts.chance@gmail.com>
2013-06-29 16:13:16 -0600

    Global Variables to Use Later!

---
+++ constant.java
@@ -0,0 +1,8 @@
+
+public class constant {
+  public static boolean leftPressed = false;
+	public static boolean rightPressed = false;
+	public static boolean downPressed = false;
+	public static boolean jumpPressed = false;
+	public static boolean runPressed = false;
+}
